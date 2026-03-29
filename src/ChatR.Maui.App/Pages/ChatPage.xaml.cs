896cbf30cb4e215c876532f359d8995fd2d1d15d
Roman Jašek <roman.jasek@hotmail.com>
2026-03-29 15:52:57 +0200

    Fix CollectionView scrolling out of the screen when keyboard is open

--- src/ChatR.Maui.App/Pages/ChatPage.xaml.cs
+++ src/ChatR.Maui.App/Pages/ChatPage.xaml.cs
@@ -1,3 +1,5 @@
+using System.Collections.Specialized;
+
 namespace ChatR.Maui.App.Pages;
 
 public partial class ChatPage : ContentPageBase<ChatPageViewModel>
@@ -5,5 +7,23 @@ public partial class ChatPage : ContentPageBase<ChatPageViewModel>
     public ChatPage(ChatPageViewModel viewModel) : base(viewModel)
     {
         InitializeComponent();
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        ViewModel.Messages.CollectionChanged += OnMessagesChanged;
+    }
+
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        ViewModel.Messages.CollectionChanged -= OnMessagesChanged;
+    }
+
+    private void OnMessagesChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.Action == NotifyCollectionChangedAction.Add && ViewModel.Messages.Count > 0)
+            Dispatcher.Dispatch(() => MessagesCollectionView.ScrollTo(ViewModel.Messages[^1], position: ScrollToPosition.End, animate: false));
     }
 }
--- src/ChatR.Maui.App/Platforms/Android/MainActivity.cs
+++ src/ChatR.Maui.App/Platforms/Android/MainActivity.cs
@@ -1,10 +1,16 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace ChatR.Maui.App;
 
 [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
 public class MainActivity : MauiAppCompatActivity
 {
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+        Window?.SetSoftInputMode(SoftInput.AdjustResize);
+    }
 }
