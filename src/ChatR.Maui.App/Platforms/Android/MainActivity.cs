5f3344d8f26a0288c54b642181952c228139b3ec
Roman Jašek <roman.jasek@hotmail.com>
2026-03-29 16:10:36 +0200

    Add implementation of fix 09

--- src/ChatR.Maui.App/Platforms/Android/MainActivity.cs
+++ src/ChatR.Maui.App/Platforms/Android/MainActivity.cs
@@ -1,7 +1,7 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
-using Android.Views;
+using AndroidX.Core.View;
 
 namespace ChatR.Maui.App;
 
@@ -11,6 +11,20 @@ public class MainActivity : MauiAppCompatActivity
     protected override void OnCreate(Bundle? savedInstanceState)
     {
         base.OnCreate(savedInstanceState);
-        Window?.SetSoftInputMode(SoftInput.AdjustResize);
+        ViewCompat.SetOnApplyWindowInsetsListener(Window!.DecorView, new KeyboardInsetsListener());
+    }
+
+    private class KeyboardInsetsListener : Java.Lang.Object, IOnApplyWindowInsetsListener
+    {
+        public WindowInsetsCompat OnApplyWindowInsets(Android.Views.View v, WindowInsetsCompat insets)
+        {
+            var imeInsets = insets.GetInsets(WindowInsetsCompat.Type.Ime());
+            var navBarInsets = insets.GetInsets(WindowInsetsCompat.Type.NavigationBars());
+            var keyboardPadding = Math.Max(0, imeInsets.Bottom - navBarInsets.Bottom);
+
+            v.FindViewById(Android.Resource.Id.Content)?.SetPadding(0, 0, 0, keyboardPadding);
+
+            return ViewCompat.OnApplyWindowInsets(v, insets);
+        }
     }
 }
